c5a2ca3d2a3ca5fb2aa0a10666911da505532484
namanshharma <namansharma812@gmail.com>
2024-09-30 02:15:33 -0700

    Initial Commit of React Router exercise

--- 04_React_Router/src/main.jsx
+++ 04_React_Router/src/main.jsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout'
+import Home from './Components/Home/Home'
 import './index.css'
+import AboutUs from './Components/About_us/About_Us'
+import ContactUs from './Components/Contact_us/Contact_Us'
+
+const router = createBrowserRouter([
+  {
+    path: '/', element: Layout, children: [
+      { path: '', element: <Home /> },
+      { path: 'About', element: <AboutUs /> },
+      { path: 'Contact', element: <ContactUs /> }
+    ]
+  }
+])
+
 
 createRoot(document.getElementById('root')).render(
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>,
+
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />        // 
+  </StrictMode>
 )
