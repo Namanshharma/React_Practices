b8dc3ead642fcd80bfe86b33e722ec3208e70a2f
namanshharma <namansharma812@gmail.com>
2024-10-03 00:17:38 +0530

    React Router

--- 04_React_Router/src/App.jsx
+++ 04_React_Router/src/App.jsx
@@ -1,14 +1,8 @@
 import './App.css'
-import Header from './Components/Header/Header'
-import Home from './Components/Home/Home'
 
 function App() {
   return (
     <>
-      <h1 className='bg-orange-600 p-4'>React Router</h1>
-      <Header></Header>
-      <Home></Home>
-      Foo
     </>
   )
 }
--- 04_React_Router/src/main.jsx
+++ 04_React_Router/src/main.jsx
@@ -7,9 +7,10 @@ import './index.css'
 import AboutUs from './Components/About_us/About_Us'
 import ContactUs from './Components/Contact_us/Contact_Us'
 
+// this mentioned below way is the first way to create a router.
 const router = createBrowserRouter([
   {
-    path: '/', element: Layout, children: [
+    path: '/', element: <Layout />, children: [
       { path: '', element: <Home /> },
       { path: 'About', element: <AboutUs /> },
       { path: 'Contact', element: <ContactUs /> }
@@ -24,6 +25,6 @@ createRoot(document.getElementById('root')).render(
   // </StrictMode>,
 
   <StrictMode>
-    <RouterProvider router={router} />        // 
+    <RouterProvider router={router} />        {/*here router is variable which we need to create it as well as there are 2 ways to create that */}
   </StrictMode>
 )
