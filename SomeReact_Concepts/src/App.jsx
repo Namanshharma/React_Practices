cd5e45313089ce4906965ae36b6bd5c7e40615f2
Naman Sharma <namansharma812@gmail.com>
2025-05-05 02:42:00 +0530

    Wrapper Components

--- SomeReact_Concepts/src/App.jsx
+++ SomeReact_Concepts/src/App.jsx
@@ -1,36 +1,71 @@
-import { useState } from 'react'
 import './App.css'
 
-export default function App() {
-  const [todo, setToDo] = useState([
-    { id: 1, title: "Go to Gym", description: "Go to gym" },
-    { id: 2, title: "Go to Kulcha land", description: "Go to Kulcha Land" },
-    { id: 3, title: "Go to Darshan", description: "Go to Darshan" }
-  ]);
+//#region Concept of Wrapper Component
+// this one is the most optimized way to use the Wrapper components
+// export default function App() {
+//   return <>
+//     <WrapperComponent>
+//       <InnerComponent></InnerComponent>
+//       Hi There !!!!!......
+//     </WrapperComponent>
+//     <WrapperComponent>
+//       <InnerComponent></InnerComponent>
+//       Hi There !!!!!......
+//     </WrapperComponent>
+//     <WrapperComponent>
+//       <InnerComponent></InnerComponent>
+//       Hi There !!!!!......
+//     </WrapperComponent>
+//   </>
+// }
+// const WrapperComponent = ({ children }) => {
+//   return <div style={{ border: "2px solid Black", height: "100px" }}>{children}</div>
+// }
+// const InnerComponent = () => {
+//   return <div>Hi from Inner Component</div>
+// }
 
-  return (  // this same spread operator thing we can done by using the Push method
-    <>
-      <button onClick={() => { setToDo([...todo, { id: 4, title: "Added Random To Do", description: "Added Random Description" }]) }}>Add a new To Do</button>
-      {todo.map(async (x) => <ToDo title={x.title} description={x.description}></ToDo>)}
-    </>
-  )
-}
+// this one is one of the way to create wrapper
+// export default function App() {
+//   return (
+//     <> <CardWrapper innerComponent={<InnerMaterialOfWrapperComponent></InnerMaterialOfWrapperComponent>}></CardWrapper> </>
+//   )
+// }
+// const CardWrapper = ({ innerComponent }) => {
+//   return <div style={{ border: "2px solid Black", height: "100px" }}>{innerComponent} </div>
+// }
+// const InnerMaterialOfWrapperComponent = () => {
+//   return <h1> Hi from Inner Wrapper Component </h1>
+// }
+//#endregion
 
-const ToDo = ({ title, description }) => {
-  return (
-    <>
-      <h1>{title}</h1>
-      <h3>{description}</h3>
-    </>
-  )
-}
+//#region -- Concept of Key
+// let count = 4;
+// export default function App() {
+//   const [todo, setToDo] = useState([
+//     { id: 1, title: "Go to Gym", description: "Go to gym" },
+//     { id: 2, title: "Go to Kulcha land", description: "Go to Kulcha Land" },
+//     { id: 3, title: "Go to Darshan", description: "Go to Darshan" }
+//   ]);
 
-
-
-
-
-
-
+//   return (  // this same spread operator thing we can done by using the Push method
+//     <>
+//       <button onClick={() => { setToDo([...todo, { id: count++, title: "Added Random To Do", description: "Added Random Description" }]) }}>Add a new To Do</button>
+//       {todo.map(x => <ToDo key={x.id} id={x.id} title={x.title} description={x.description}></ToDo>)}
+//     </>
+//   )
+// }
+// const ToDo = ({ id, title, description }) => {
+//   return (
+//     <>
+//       <h1>{id}</h1>
+//       <h2>{title}</h2>
+//       <h3>{description}</h3>
+//     </>
+//   )
+// }
+// Concept :- Key -- By using this Key attribute in the component, react will know that sequence of the objects in the array. Basically, more optimally things will render here.
+//#endregion
 
 //#region :- This region used for 1st and 2nd re-render point
 // function App() {
