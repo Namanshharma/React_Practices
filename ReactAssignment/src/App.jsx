a14e0aaea1aa86a460167b89ce3de923d59f2c23
namanshharma <namansharma812@gmail.com>
2025-05-11 18:52:23 +0530

    Assignment one of Use Memo

---
+++ ReactAssignment/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import CalculateFactorial from './Components/CalculateFactorial'
+
+function App() {
+
+  return (
+    <>
+      <CalculateFactorial></CalculateFactorial>
+    </>
+  )
+}
+
+export default App
